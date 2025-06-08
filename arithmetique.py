149ff513f0473ce8095d5161a5460c6c95004e52
Pierre-Loup Martin <pierre-loupmartin@mac-1.home>
2025-06-08 18:47:15 +0200

    Ajout de mon premier fichier depuis le dossier 'python'

---
+++ arithmetique.py
@@ -0,0 +1,27 @@
+x, y = 1, 2.5
+a = True 
+
+#Arithmetique 
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+
+#Comparaison
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
+
+#Logique
+
+print(False & True) #And
+print(False | True) #OR
+print(False ^ True) #XOR
+
+#String 
+
+prenom = 'Guillaume, 1'
+print(prenom)
+
